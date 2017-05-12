1d555d395df9ba40b7e1228e174a81931c241964
Martin Suesskraut <martin.suesskraut@gmail.com>
2017-05-12 20:43:16 +0200

    finished interpreter test

--- src/interp_tests.rs
+++ src/interp_tests.rs
@@ -58,3 +58,129 @@ fn dec_wrapping() {
 fn move_right() {
     execute_and_check(&[Inc(2), MoveRight(1), Inc(1), Output], "", &[1]);
 }
+
+#[test]
+fn move_left() {
+    execute_and_check(&[Inc(2), MoveRight(1), Inc(1), Output, MoveLeft(1), Output],
+                      "",
+                      &[1, 2]);
+}
+
+#[test]
+fn move_right_2() {
+    execute_and_check(&[Inc(3),
+                        MoveRight(1),
+                        MoveRight(1),
+                        Inc(1),
+                        Output,
+                        MoveLeft(1),
+                        Output,
+                        MoveLeft(1)],
+                      "",
+                      &[1, 0]);
+}
+
+#[test]
+fn move_right_2_at_once() {
+    execute_and_check(&[Inc(3),
+                        MoveRight(2),
+                        Inc(1),
+                        Output,
+                        MoveLeft(1),
+                        Output,
+                        MoveLeft(1)],
+                      "",
+                      &[1, 0]);
+}
+
+#[test]
+fn move_left_2() {
+    execute_and_check(&[Inc(2),
+                        MoveRight(2),
+                        Inc(1),
+                        Output,
+                        MoveLeft(1),
+                        MoveLeft(1),
+                        Output],
+                      "",
+                      &[1, 2]);
+}
+
+#[test]
+fn move_left_2_at_once() {
+    execute_and_check(&[Inc(2),
+                        MoveRight(2),
+                        Inc(1),
+                        Output,
+                        MoveLeft(1),
+                        MoveLeft(1),
+                        Output],
+                      "",
+                      &[1, 2]);
+}
+
+#[test]
+fn input() {
+    execute_and_check(&[Input, Output, Input, Output], "a1", &[b'a', b'1']);
+}
+
+#[test]
+fn loop_set_zero() {
+    execute_and_check(&[Inc(100), LoopEntry(3), Dec(1), LoopExit(1), Output],
+                      "",
+                      &[0]);
+}
+
+#[test]
+fn loop_move_value() {
+    execute_and_check(&[Inc(100),
+                        LoopEntry(6),
+                        Dec(1),
+                        MoveRight(3),
+                        Inc(1),
+                        MoveLeft(3),
+                        LoopExit(1),
+                        Output,
+                        MoveRight(3),
+                        Output],
+                      "",
+                      &[0, 100]);
+}
+
+#[test]
+fn loop_find_zero_left() {
+    execute_and_check(&[MoveRight(1),
+                        Inc(1),
+                        MoveRight(1),
+                        Inc(1),
+                        MoveRight(1),
+                        Inc(1),
+                        LoopEntry(8),
+                        MoveLeft(1),
+                        LoopExit(6),
+                        Output],
+                      "",
+                      &[0]);
+}
+
+#[test]
+fn loop_find_zero_right() {
+    execute_and_check(&[MoveRight(1),
+                        Inc(1),
+                        MoveRight(1),
+                        Inc(1),
+                        MoveRight(1),
+                        Inc(1),
+                        LoopEntry(8),
+                        MoveLeft(1),
+                        LoopExit(6),
+                        Output,
+                        Inc(9),
+                        Output,
+                        LoopEntry(14),
+                        MoveRight(1),
+                        LoopExit(12),
+                        Output],
+                      "",
+                      &[0, 9, 0]);
+}
